126c6803d393b98a3a0b5f7e910e6078b4649ec0
Matej <matej.janacek@gmail.com>
2013-09-15 00:09:20 +0200

    Some changes to canvas demo

--- canvasrender.js
+++ canvasrender.js
@@ -10,24 +10,47 @@ gameGrid.renderer = (function (){
 
             setCanvasSize(canvas, fieldSize);
             drawGrid(context, this.width, this.height, fieldSize);
+
+            for(var i = 0; i < this.grid.length; i++) {
+                for (var j = 0; j < this.grid[i].length; j++) {
+                    if (this.grid[i][j]) {
+                        drawPiece(context, i, j, fieldSize);
+                    }
+                }
+            }
         }
     };
 
+    var drawPiece = function(context, x, y, size) {
+        var dx = (x * size) + (size/2);
+        var dy = (y * size) + (size/2);
+        var radius = (size/2) - (size/5);
+
+        context.beginPath();
+        context.arc(dx, dy, radius, 0, Math.PI*2, false);
+        context.closePath();
+
+        context.strokeStyle = "#333";
+        context.stroke();
+        context.fillStyle = "#000";
+        context.fill();
+    }
+
     var setCanvasSize = function(canvas, fieldSize) {
-        canvas.width = this.width * fieldSize;
-        canvas.height = this.height * fieldSize;
+        canvas.width = this.width * fieldSize + 1;
+        canvas.height = this.height * fieldSize + 1;
     };
 
     var drawGrid = function(context, width, height, size) {
         for (var x = 0; x <= width; x++) {
-            context.moveTo(x * size, 0);
-            context.lineTo(x * size, height * size);
+            context.moveTo(0.5 + x * size, 0);
+            context.lineTo(0.5 + x * size, 1 + height * size);
         }
 
 
         for (var y = 0; y <= height; y++) {
-            context.moveTo(0, y * size);
-            context.lineTo(width * size, y *size);
+            context.moveTo(0, 0.5 + y * size);
+            context.lineTo(1 + width * size, 0.5 + y *size);
         }
 
         context.strokeStyle = "black";
--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,2 +1,15 @@
 gameGrid.init(10, 10, "demoCanvas");
+// set some pieces black
+gameGrid.setPiece(1, 0, 1);
+gameGrid.setPiece(2, 3, 1);
+gameGrid.setPiece(2, 8, 1);
+gameGrid.setPiece(3, 4, 1);
+gameGrid.setPiece(5, 1, 1);
+gameGrid.setPiece(6, 6, 1);
+gameGrid.setPiece(9, 2, 1);
+gameGrid.setPiece(9, 9, 1);
+gameGrid.setPiece(0, 5, 1);
+gameGrid.setPiece(6, 7, 1);
+gameGrid.setPiece(7, 7, 1);
+
 gameGrid.renderer.draw("demoCanvas");
