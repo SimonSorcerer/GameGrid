4a305588f40963ba8085c863dc8811f9feffac5c
Matej <matej.janacek@gmail.com>
2013-09-16 14:05:09 +0200

    Encapsulating module variables, adding basic players logic

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,5 +1,5 @@
 gameGrid.init(10, 10, "demoCanvas");
-// set some pieces black
+// set some black pieces
 gameGrid.setPiece(1, 0, 1);
 gameGrid.setPiece(2, 3, 1);
 gameGrid.setPiece(2, 8, 1);
@@ -11,5 +11,19 @@ gameGrid.setPiece(9, 9, 1);
 gameGrid.setPiece(0, 5, 1);
 gameGrid.setPiece(6, 7, 1);
 gameGrid.setPiece(7, 7, 1);
+
+// set some white pieces
+gameGrid.setPiece(1, 2, 0);
+gameGrid.setPiece(3, 3, 0);
+gameGrid.setPiece(3, 9, 0);
+gameGrid.setPiece(4, 9, 0);
+gameGrid.setPiece(5, 5, 0);
+gameGrid.setPiece(8, 0, 0);
+
+// some red
+gameGrid.setPiece(6, 8, 2);
+gameGrid.setPiece(4, 2, 2);
+gameGrid.setPiece(4, 8, 2);
+gameGrid.setPiece(7, 1, 2);
 
 gameGrid.renderer.draw("demoCanvas");
--- gamegrid.js
+++ gamegrid.js
@@ -2,6 +2,7 @@ var gameGrid = (function () {
     var width, height;
     var canvas;
     var grid = [];
+    var players = [];
 
     // public methods
     // --------------------------------------------------------------
@@ -10,43 +11,62 @@ var gameGrid = (function () {
         this.setDimensions(width, height);
         this.setCanvasId(elementId);
 
-        this.grid = initGrid(this.width, this.height);
+        grid = initGrid(width, height);
     }
 
-    this.setDimensions = function(width, height) {
-        this.width = width;
-        this.height = height;
+    this.setDimensions = function(w, h) {
+        width = w;
+        height = h;
     };
 
+    this.getDimensions = function() {
+        return {
+            width: width,
+            height: height
+        }
+    }
+
     this.setCanvasId = function(elementId) {
-        this.canvas = elementId;
+        canvas = elementId;
     };
 
     this.getPiece = function(x, y) {
         if (pieceExists(x, y)) {
-            return this.grid[x][y];
+            return grid[x][y];
         }
     }
 
     this.setPiece = function(x, y, val) {
         if (pieceExists(x, y)) {
-            this.grid[x][y] = val;
+            grid[x][y] = val;
+
+            // if player for this piece doesn't exist, create it (and all players up to number val)
+            while (!playerExists(val)) {
+                this.addPlayer();
+            }
+
             return true;
         }
         return false;
     }
 
-    this.switchPiece = function (x, y) {
-        if (pieceExists(x, y)) {
-            this.grid[x][y] = 1 - this.grid[x][y];
-        }
+    this.getGrid = function() {
+        return grid;
+    }
+
+    this.addPlayer = function(playerName) {
+        players.push(playerName || 'anonymous');
     }
 
     // private methods
     // --------------------------------------------------------------
 
     var pieceExists = function(x, y) {
-        return (y < this.grid.length && x < this.grid[y].length)
+        return (y < grid.length && x < grid[y].length)
+    }
+
+    var playerExists = function(playerNumber) {
+        return players.length > playerNumber;
     }
 
     var initGrid = function(width, height) {
@@ -54,11 +74,11 @@ var gameGrid = (function () {
 
         // check if the dimensions are set
         if (width && height) {
-            // create array of arrays (width * height) and fill with zeroes
+            // create array of arrays (width * height) and fill with none (-1 here)
             for (var i = 0; i < height; i++) {
                 grid[i] = [];
                 for (var j = 0; j < width; j++) {
-                    grid[i][j] = 0;
+                    grid[i][j] = -1;
                 }
             }
         }
