25243882e69e9255f3e1f7a446031431a70f90db
Matej <matej.janacek@gmail.com>
2013-09-16 14:04:50 +0200

    Encapsulating module variables, adding basic players logic

--- canvasrender.js
+++ canvasrender.js
@@ -1,27 +1,29 @@
 gameGrid.renderer = (function (){
-    this.draw = function(canvasId) {
+    this.draw = function(canvasId, grid) {
         var canvas = document.getElementById(canvasId);
         var fieldSize = 30;
+        var grid = gameGrid.getGrid();
+        var dimensions = gameGrid.getDimensions();
 
         // check the element is in the DOM and the browser supports canvas
         if (canvas.getContext) {
             // initialise a 2-dimensional drawing context
             var context = canvas.getContext('2d');
 
-            setCanvasSize(canvas, fieldSize);
-            drawGrid(context, this.width, this.height, fieldSize);
+            setCanvasSize(canvas, dimensions.width, dimensions.height, fieldSize);
+            drawGrid(context, dimensions.width, dimensions.height, fieldSize);
 
-            for(var i = 0; i < this.grid.length; i++) {
-                for (var j = 0; j < this.grid[i].length; j++) {
-                    if (this.grid[i][j]) {
-                        drawPiece(context, i, j, fieldSize);
+            for(var i = 0; i < grid.length; i++) {
+                for (var j = 0; j < grid[i].length; j++) {
+                    if (grid[i][j] >= 0) {
+                        drawPiece(context, i, j, fieldSize, grid[i][j]);
                     }
                 }
             }
         }
     };
 
-    var drawPiece = function(context, x, y, size) {
+    var drawPiece = function(context, x, y, size, player) {
         var dx = (x * size) + (size/2);
         var dy = (y * size) + (size/2);
         var radius = (size/2) - (size/5);
@@ -30,15 +32,28 @@ gameGrid.renderer = (function (){
         context.arc(dx, dy, radius, 0, Math.PI*2, false);
         context.closePath();
 
+        context.lineWidth = 2;
         context.strokeStyle = "#333";
         context.stroke();
-        context.fillStyle = "#000";
+
+        context.fillStyle = getPlayerColor(player);
         context.fill();
     }
 
-    var setCanvasSize = function(canvas, fieldSize) {
-        canvas.width = this.width * fieldSize + 1;
-        canvas.height = this.height * fieldSize + 1;
+    var getPlayerColor = function(playerNumber) {
+        switch (playerNumber) {
+            case 0: return '#fff'; break;
+            case 1: return '#000'; break;
+            case 2: return '#f00'; break;
+            case 3: return '#0f0'; break;
+            case 4: return '#00f'; break;
+            default: return '#0ff';
+        }
+    }
+
+    var setCanvasSize = function(canvas, width, height, fieldSize) {
+        canvas.width = width * fieldSize + 1;
+        canvas.height = height * fieldSize + 1;
     };
 
     var drawGrid = function(context, width, height, size) {
